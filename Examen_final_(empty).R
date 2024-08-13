02f0c753dc323587f66626d08822ce71382080c6
Irwing S. Saldaña <52083246+irwingss@users.noreply.github.com>
2024-08-13 18:17:22 -0500

    Add files via upload

---
+++ Examen_final_(empty).R
@@ -0,0 +1,118 @@
+#
+# ███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ ██╗  ██╗
+# ████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗╚██╗██╔╝
+# ██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝ ╚███╔╝ 
+# ██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗ ██╔██╗ 
+# ██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║██╔╝ ██╗
+# ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
+#  https://www.masterx.academy/
+#  ---------------------------------------------------------------------- -
+#  EXAMEN FINAL                                                            ----
+#  ---------------------------------------------------------------------- -
+#  MÓDULO: Introducción a R y RStudio para Ciencias.                     
+#  INSTRUCCIONES: Desarrolla el examen poniendo en juego los conceptos y 
+#  funciones que aprendiste durante las clases. Toma en consideración usar
+#  en la medida de lo posible soluciones donde apliques tidyverse, indexación
+#  operadores lógicos y relacionales, y demás códigos.
+#  ---------------------------------------------------------------------- -
+#  ---------------------------------------------------------------------- -
+
+#  ---------------------------------------------------------------------- -
+# 1. Carga tu base de datos del diplomado ---------------------------------
+#  ---------------------------------------------------------------------- -
+## Puntaje: 2 puntos
+
+## Instrucción: Utiliza las funciones de carga de base de datos que sea la
+## adecuada para la extensión de tu base de datos (.csv, .txt, .xlsx).
+## Carga solo una vez tu base. Dale un nombre y úsala a lo largo del examen.
+#  ---------------------------------------------------------------------- -
+## Desarrollo:
+
+
+
+
+
+
+
+
+#  ---------------------------------------------------------------------- -
+# 2. Aplica tres (03) filtrados con tidyverse y R Base --------------------
+#  ---------------------------------------------------------------------- -
+## Puntaje: 6 puntos
+
+## Instrucción: Usa la base tu datos y crea tres (03) pipelines de filtrado
+## usando la función respectiva de tidyverse. De bajo de cada pipeline, crea
+## una versión del mismo código pero usando index y el operador de indexación
+## clásico para tu data.frame.
+#  ---------------------------------------------------------------------- -
+## Desarrollo:
+
+
+
+
+
+
+
+
+#  ---------------------------------------------------------------------- -
+# 3. Aplica dos (02) veces selección columnas con tidyverse y R Base ------
+#  ---------------------------------------------------------------------- -
+## Puntaje: 4 puntos
+
+## Instrucción: Usa la base tu datos y crea dos (02) pipelines de selección
+## de columnas usando la función respectiva de tidyverse. Al menos uno de los
+## códigos de selección debe usar selección condicional con select_if().
+## De bajo de cada pipeline, crea una versión del mismo código pero usando 
+## index y el operador de indexación clásico para tu data.frame.
+#  ---------------------------------------------------------------------- -
+## Desarrollo:
+
+
+
+
+
+
+
+
+#  ---------------------------------------------------------------------- -
+# 4. Crea tres (03) resúmenes estadísticos relevantes ---------------------
+#  ---------------------------------------------------------------------- -
+## Puntaje: 6 puntos
+
+## Instrucción: Usa la base tu datos y crea tres (03) pipelines en las que 
+## crees un resumen estadístico por grupo (una variable de agrupamiento es 
+## suficiente). SOLAMENTE si no cuentas con una variable categórica en tu 
+## dataframe, puedes crear una ficticia con los códigos de creación de
+## vectores repetidos. Para tus tres pipelines, la función de reordenamiento
+## de filas en base a cualquiera de tus columnas numéricas. Finalmente, no
+## olvides desagrupar tu código.
+#  ---------------------------------------------------------------------- -
+## Desarrollo:
+
+
+
+
+
+
+
+
+#  ---------------------------------------------------------------------- -
+# 5. Crea dos (02) gráficos con R base ------------------------------------
+#  ---------------------------------------------------------------------- -
+## Puntaje: 2 puntos
+
+## Instrucción: Usa tu base de datos y crea dos gráficos de tu preferencia.
+## Es mucho mejor si estos gráficos reflejan relación entre dos variables de 
+## interés tuyo (scatterplot de dos variables que pretendes luego correlacionar
+## más adelante en el diplomado; o una variable categórica sobre la que quieres 
+## ver los boxplots en base a otra variable numérica. Se creativ@). Bríndale
+## títulos y colores.
+#  ---------------------------------------------------------------------- -
+## Desarrollo:
+
+
+
+
+
+
+
